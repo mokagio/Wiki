28042f22d3f8c434cf4aa6c46896a7ae2f7f62f8
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-11-19 22:29:15 +1100

    Remove Xcode boilerplate from Examples app files

--- Documentation Examples/Examples/ViewController.swift
+++ Documentation Examples/Examples/ViewController.swift
@@ -1,25 +1,3 @@
-//
-//  ViewController.swift
-//  Examples
-//
-//  Created by Giovanni on 19/11/16.
-//
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
-
+class ViewController: UIViewController { }
